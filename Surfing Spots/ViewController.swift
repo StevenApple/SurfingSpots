13c3bd8a95ecd242eda83dc24af7c2e18250293a
Steven <steven@anappleapp.com>
2020-07-31 15:40:58 +0200

    Initial Commit

---
+++ Surfing Spots/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Surfing Spots
+//
+//  Created by Steven M on 31/07/2020.
+//  Copyright © 2020 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
