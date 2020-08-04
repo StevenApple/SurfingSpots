c7fdcadb26fb3b0a4488be9d208bcb49103ed1c5
Steven <steven@anappleapp.com>
2020-08-04 15:00:41 +0200

    Url constant modified
    The constant name of url changed from "downloadTheListOfCity" to "urlRunMocky"

--- Surfing Spots/App URL Config/AppUrlConfig.swift
+++ Surfing Spots/App URL Config/AppUrlConfig.swift
@@ -2,5 +2,5 @@
 import UIKit
 
 struct UrlConfig {
-    static let downloadTheListOfCity = "https://run.mocky.io/v3/652ceb94-b24e-432b-b6c5-8a54bc1226b6"
+    static let urlRunMocky = "https://run.mocky.io/v3/652ceb94-b24e-432b-b6c5-8a54bc1226b6"
 }
