2d35e9211bc49a4960a8294288208550caf1145e
Steven <steven@anappleapp.com>
2020-08-05 22:02:40 +0200

    Parsing data in TableViewController

--- Surfing Spots/Controller/TableViewController.swift
+++ Surfing Spots/Controller/TableViewController.swift
@@ -4,83 +4,61 @@ import UIKit
 class TableViewController: UITableViewController,NetworkDownloadProtocols {
 
     var listOfCity = [CitydWithTemperature]()
+    var previousListOfCity = [CitydWithTemperature]()
+    var randomNumbers = [Int]()
+   
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       self.navigationController?.configNavigationBarAppearance()
-        downloadTheListOfCity(from: URL(string: UrlConfig.downloadTheListOfCity)!) { listOfCityWithTemperature in
-            self.listOfCity = listOfCityWithTemperature
-          
-          
+        self.navigationController?.configNavigationBarAppearance()
+        self.downloadTheListOfCity{ success in
+            
+            if (success){
+                print("Timer fired")
+                _ =  Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.updateRandomlyEveryThreeSecondsTheTemperature), userInfo: nil, repeats: true)
+            }
         }
-
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+
         return self.listOfCity.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        var cell : TableViewCell! = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableViewCell
+        
+        if (cell == nil){
+            
+            let nib : [TableViewCell] = Bundle.main.loadNibNamed("Cell", owner: self, options: nil) as! [TableViewCell]
+            cell = nib[0]
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        }
+        cell.cityLabel.text = self.listOfCity[indexPath.row].city
+        print("The city name on cell \(String(describing: cell.cityLabel.text)) and temp \(self.listOfCity[indexPath.row].temperature)")
+        
+        if (self.listOfCity[indexPath.row].cloudy){
+            cell.temperatureLabel.text = "Cloudy" + " - " + String(self.listOfCity[indexPath.row].temperature) + " " + "degrees"
+            cell.cloudyView.backgroundColor = UIColor.darkGray
+            cell.cloudyView.makeTheViewCornersRounded(targetView: cell.cloudyView)
 
-        // Configure the cell...
-
+        } else {
+            cell.temperatureLabel.text = "Sunny" + " - " + String(self.listOfCity[indexPath.row].temperature) + " " + "degrees"
+            cell.img.image = UIImage(named: self.listOfCity[indexPath.row].city)?.makeCornersRounded(radius: 20)
+            
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
+  
 }
