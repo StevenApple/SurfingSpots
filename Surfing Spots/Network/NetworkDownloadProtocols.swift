1506e6951c8c71bd3edd944ff8f33775b714d649
Steven <steven@anappleapp.com>
2020-08-05 22:06:54 +0200

    Adding Get request in order to generate random number.

--- Surfing Spots/Network/NetworkDownloadProtocols.swift
+++ Surfing Spots/Network/NetworkDownloadProtocols.swift
@@ -3,6 +3,7 @@ import UIKit
 
 protocol NetworkDownloadProtocols{
     func downloadTheListOfCity(from url: URL, completion:@escaping([CitydWithTemperature]) -> Void)
+    func generateRandomNumberFromApiThenAssignRandomlyToTemperature(from url: URL,completion:@escaping(Int,Bool,Bool) -> Void)
 }
 
 
@@ -34,7 +35,7 @@ extension NetworkDownloadProtocols {
                                     ))
                                 }
                                
-                                completion(listOfCitiedWithTemperature.sorted())
+                                completion(listOfCitiedWithTemperature)
                             }
                         }
                     }catch let error as NSError {
@@ -46,5 +47,58 @@ extension NetworkDownloadProtocols {
             }.resume()
         }
     }
+    
+}
+
+extension NetworkDownloadProtocols{
+    
+    func generateRandomNumberFromApiThenAssignRandomlyToTemperature(from url: URL, completion:@escaping(Int,Bool,Bool) -> Void){
+        
+        var cloudy = false
+        
+        let session = URLSession(configuration: .default)
+        var request = URLRequest.init(url: url)
+        request.addValue("application/json", forHTTPHeaderField:"Content-Type")
+        request.httpMethod = "GET"
+        request.timeoutInterval = 30.0
+        
+        DispatchQueue.global(qos: .background).async {
+            print("In background")
+            
+            session.dataTask(with:request) { data, response, error in
+                if error != nil {
+                    print(error?.localizedDescription ?? "Unknown error")
+                }
+                if let data = data{
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            if let temp = json["number"] as? Int{
+                                print("What is temp \(temp)")
+                                if (temp < 30){
+                                    cloudy = true
+                                } else {
+                                    cloudy = false
+                                }
+      
+                                completion(temp,cloudy,true)
+                                
+                   
+                            } else {
+                                print("Failed to unwrap number from json")
+                            }
+
+                        }
+                    }catch let error as NSError {
+                        print("Failed to load: \(error.localizedDescription)")
+                    }
+                } else {
+                    
+                }
+                
+            }.resume()
+        }
+    }
+    
+    
 }
 
