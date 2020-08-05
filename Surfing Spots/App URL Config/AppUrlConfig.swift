151eaa96db4ef4c9f42335debb76d350ac9fc729
Steven <steven@anappleapp.com>
2020-08-05 22:09:52 +0200

    Added Url for Get request.

--- Surfing Spots/App URL Config/AppUrlConfig.swift
+++ Surfing Spots/App URL Config/AppUrlConfig.swift
@@ -3,4 +3,5 @@ import UIKit
 
 struct UrlConfig {
     static let urlRunMocky = "https://run.mocky.io/v3/652ceb94-b24e-432b-b6c5-8a54bc1226b6"
+    static let numbersApi = "http://numbersapi.com/random?min=10&max=60"
 }
